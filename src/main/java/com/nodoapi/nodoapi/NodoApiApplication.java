93c019f0d226797edb20e4c9845fcf5206db94fe
FelipeArredond <pipechiqui77@gmail.com>
2023-06-26 07:39:58 -0500

    Swagger Doc Added

--- src/main/java/com/nodoapi/nodoapi/NodoApiApplication.java
+++ src/main/java/com/nodoapi/nodoapi/NodoApiApplication.java
@@ -1,8 +1,4 @@
 package com.nodoapi.nodoapi;
-
-import com.nodoapi.nodoapi.persistence.entity.Rol;
-import com.nodoapi.nodoapi.persistence.repository.RolRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
